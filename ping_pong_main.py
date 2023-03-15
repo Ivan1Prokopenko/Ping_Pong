668776a56c449c0cc734bec4480b6449de66fd46
Ivan1Prokopenko <127607512+Ivan1Prokopenko@users.noreply.github.com>
2023-03-15 19:16:38 +0100

    Update ping_pong_main.py
    Added UI interface to the game

--- ping_pong_main.py
+++ ping_pong_main.py
@@ -25,3 +25,39 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_width-80:
             self.rect.y += self.speed
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player("racket.png", 30, 200, 50, 150, 4)
+racket2 = Player("racket.png", 520, 200, 50, 150, 4)
+ball = GameSprite("tenis_ball.png", 200, 200, 50, 50, 4)
+
+font.init()
+font = font.SysFont("Arial", 35)
+lose1 = font.render("Player 1 LOSE!", True, (180, 0, 0))
+lose2 = font.render("Player 2 LOSE!", True, (180, 0, 0))
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
